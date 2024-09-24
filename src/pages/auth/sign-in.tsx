11a164b846a6016ad4d126258139f7dfb73c8aae
Betiza Barreira <be.penques@hotmail.com>
2024-09-24 16:00:14 -0300

    css pagina de cadastro novo restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,12 +25,19 @@ export function SignIn() {
   async function handleSignIn(data: signInFormType) {
     console.log(data)
     await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('Enviamos um link de autenticação para seu e-mail')
   }
 
   return (
     <>
       <Helmet title="login" />
       <div className="p-8">
+        <Button variant="ghost" asChild>
+          <Link to="/sign-up" className="absolute right-8 top-8">
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
