9b270404695794ef25c4f28b15d971746198a093
Betiza Barreira <be.penques@hotmail.com>
2024-09-24 19:53:15 -0300

    layout do app com cabeçalho

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
 
-export function AppLayout(){
-    return(
-        <div>
-            <h1>Cabeçalho</h1>
+import { Header } from '@/components/header'
 
-            <div><Outlet/></div>
-        </div>
-    )
+export function AppLayout() {
+  return (
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
+    </div>
+  )
 }
