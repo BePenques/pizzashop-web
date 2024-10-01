fac2ffde8a26f8fa2648baa2d29ecbbf72ad8d2f
Betiza Barreira <be.penques@hotmail.com>
2024-10-01 11:58:43 -0300

    perfil no cabeçalho + loading state no perfil

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,13 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true, // faz os cookies do front sejam enviados pro back
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
